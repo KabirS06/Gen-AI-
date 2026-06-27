e2af71dd53662c0eb0817c8c24e6b499371488b4
unknown <kabirsharma4406@gmail.com>
2026-06-27 21:17:58 +0530

    LLMs

--- LangChain/01_Chat_Models/LLMs/llm_demo.py
+++ LangChain/01_Chat_Models/LLMs/llm_demo.py
@@ -1,10 +1,10 @@
 from langchain_openai import OpenAI
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() # load .env file and connect API key
 
-llm=OpenAI(model='gpt-3.5-turbo-instruct')
+llm=OpenAI(model='gpt-3.5-turbo-instruct') # model name 
 
-result=llm.invoke("what is the value of pi till the 10th decimal value")
+result=llm.invoke("what is the value of pi till the 10th decimal value") # return the result of the query
 
 print(result)
