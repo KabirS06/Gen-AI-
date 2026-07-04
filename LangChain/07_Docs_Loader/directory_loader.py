f5239538c896bc20cb94f5bad5f95de36c96e2c6
unknown <kabirsharma4406@gmail.com>
2026-07-05 02:04:41 +0530

    directory_loader

---
+++ LangChain/07_Docs_Loader/directory_loader.py
@@ -0,0 +1,17 @@
+from langchain_community.document_loaders import PyPDFLoader , DirectoryLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+
+load_dotenv()
+
+loader=DirectoryLoader(
+    path='books',
+    glob='*.pdf',
+    loader_cls=PyPDFLoader
+)
+
+docs=loader.load()
+
+print(len(docs))
