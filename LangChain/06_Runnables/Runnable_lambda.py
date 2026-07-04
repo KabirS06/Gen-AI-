e68a15e27d930b275d2063a8f27e6019be5f1cd8
unknown <kabirsharma4406@gmail.com>
2026-07-04 16:23:17 +0530

    Runnable_Lambda

---
+++ LangChain/06_Runnables/Runnable_lambda.py
@@ -0,0 +1,41 @@
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel ,RunnableSequence , RunnablePassthrough , RunnableLambda
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm=HuggingFaceEndpoint(model='meta-llama/Llama-3.1-8B-Instruct',
+                        task='text_generation')
+model=ChatHuggingFace(llm=llm)
+
+
+prompt1=PromptTemplate(template='Write a joke about {topic}',
+                      input_variables=['topic'])
+
+parser=StrOutputParser()
+
+prompt2=PromptTemplate(template='Explain the joke : {joke}',
+                       input_variables=['joke'])
+
+def word_counter(text):
+    return (len(text.split()))  
+joke_gen_chain=RunnableSequence(prompt1 ,model,parser)
+
+parallel_chain=RunnableParallel({
+    'joke' : RunnablePassthrough(),
+    'word_count': RunnableLambda(word_counter),
+    'explanation' : RunnableSequence(prompt2 , model , parser)
+})
+
+final_chain=RunnableSequence(joke_gen_chain , parallel_chain)
+
+result=final_chain.invoke({'topic': 'Cricket'})
+
+print("\n ------------JOKE---------\n")
+print(result['joke'])
+print("\n ------------EXPLANATION---------\n")
+print(result['explanation'])
+print("\n ------------Number of Words ---------")
+print("""{}\nWord Count: {} """.format(result['joke'],result['word_count']))
