7b8f65646118c938770b23323bc3d7ffd3ed5c9e
unknown <kabirsharma4406@gmail.com>
2026-07-05 21:09:02 +0530

    Character Based Text Splitter

---
+++ LangChain/08_Text_splitting/Length_based.py
@@ -0,0 +1,14 @@
+from langchain_classic.text_splitter import CharacterTextSplitter
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+
+loader=PyPDFLoader('D:/Clg Kabir_/Gen-AI-/Gen-AI-/LangChain/07_Docs_Loader/dl-curriculum.pdf')
+
+docs=loader.lazy_load()
+
+spliiter=CharacterTextSplitter(chunk_size=150, chunk_overlap=0 , separator='')
+
+result=spliiter.split_documents(docs)
+
+print(result[0].page_content)
